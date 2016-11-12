5a702cd92922e77e3639cfcf8a4892a1ad289d40
CheersLove <nick.zimanski@gmail.com>
2016-11-12 10:52:38 -0500

    ,mnbjnbjn

---
+++ DDR/src/org/usfirst/frc3620/DDR/subsystems/ShooterSubsystem.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc3620.DDR.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+
+public class ShooterSubsystem extends Subsystem {
+    
+	public boolean isLidUp = false;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
